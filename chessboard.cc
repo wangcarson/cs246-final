9a0f7f3aebfe7bf0c54fc2c9f6c49b416c6e91db
c326wang <c326wang@uwaterloo.ca>
2025-07-21 18:18:26 -0400

    New commit

--- chessboard.cc
+++ chessboard.cc
@@ -30,6 +30,12 @@ Piece ChessBoard::getPiece(Tile t) {
     return grid[t.row][t.col];
 }
 
+//colour accesor method
+Colour ChessBoard::getColour (Tile t){
+    return grid[t.row][t.col].colour;
+}
+
+
 bool ChessBoard::isOccupied(Tile t) {
     Piece p = getPiece(t);
     return !p.isEmpty() && !p.isInvalid();
