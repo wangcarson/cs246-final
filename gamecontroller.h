93d06cc1f4c727fec458fbb546a172c8cd2f2545
c326wang <c326wang@uwaterloo.ca>
2025-07-24 21:16:05 -0400

    Mode enum to private

--- gamecontroller.h
+++ gamecontroller.h
@@ -14,12 +14,6 @@
 #include "boardmanager.h"
 #include "player.h"
 
-// The Mode enum represents the current mode of the GameController:
-// - Setup Mode is for when we are setting up a position.
-// - Game Mode is for taking move commands from input
-// - Normal Mode is defult before we choose what other mode we want to be in.
-enum class Mode { Setup, Game, Normal, Puzzle };
-
 // implementation included as well (remove later)
 // for input and output (maybe change this)
 class GameController {
@@ -31,7 +25,13 @@ class GameController {
     std::unique_ptr<TextDisplay> td;
     std::unique_ptr<GraphicsDisplay> gd;
 
+    // The private Mode enum represents the current mode of the GameController:
+    // - Setup Mode is for when we are setting up a position.
+    // - Game Mode is for taking move commands from input
+    // - Normal Mode is defult before we choose what other mode we want to be in.
+    enum class Mode { Setup, Game, Normal, Puzzle };
     Mode mode = Mode::Normal;
+
     int turnNumber = 1;
     bool debug = false;
     bool useGD = false;
